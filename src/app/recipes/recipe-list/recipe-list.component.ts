0cb7d9b6d99a06363bfeab9282acf7feebdfbe15
DavidKindler <kindlerdavid@gmail.com>
2016-12-23 16:06:38 -0600

    Add recipe list, shopping list, dropdown directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,20 +4,24 @@ import {Recipe} from '../recipe';
 // import { RecipeDetailComponent } from '../recipe-detail/recipe-detail.component';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'rb-recipe-list',
-  templateUrl: './recipe-list.component.html'
+  templateUrl :  './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[]  = [];
+  recipes: Recipe[]  = [
+    new Recipe('Brisket', 'Yummy', '//placehold.it/200x100',[]),
+    new Recipe('Turkey', 'Nummy', '//placehold.it/200x100',[])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  recipe= new Recipe('Dummy','Dummy','//placehold.it/200x100');
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // tslint:disable-next-line:one-line
   onSelected(recipe: Recipe){
     this.recipeSelected.emit(recipe);
   }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../ingredient';
 
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-
+  items: Ingredient[] = [
+    new Ingredient('turkey',10),
+    new Ingredient('brisket',20)
+  ];
+ 
   constructor() { }
 
   ngOnInit() {
